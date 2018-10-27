1a36ab790492311d6e8af3a6d4565981a413cab2
Andrea Jane <ajcchiap@gmail.com>
2018-10-27 16:57:24 -0400

    for shame - check for mysqli connection error - misspelling fixed

--- inc/dbConnect.php
+++ inc/dbConnect.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = new mysqli('localhost', 'root', 'root', 'nycda');
-if ($conn->connect-errno) {
+if ($conn->connect_errno) {
 	echo "Connection Failed: " . $conn->connect_error;
 	exit; //die("Fatal Error");
 }
